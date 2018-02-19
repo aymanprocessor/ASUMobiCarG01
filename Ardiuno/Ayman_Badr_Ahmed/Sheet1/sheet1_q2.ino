bab3715fbb368b827ba93b79a121e96ee854b8f4
ayman-badr <36260085+ayman-badr@users.noreply.github.com>
2018-02-19 20:18:16 +0200

    Add files via upload

---
+++ Ardiuno/Ayman_Badr_Ahmed/Sheet1/sheet1_q2.ino
@@ -0,0 +1,22 @@
+void setup()
+ { // put your setup code here, to run once:
+ pinMode(4,OUTPUT);
+ pinMode(5,OUTPUT);
+ pinMode(6,OUTPUT);
+ pinMode(7,OUTPUT);
+ }
+
+void loop()
+ { // put your main code here, to run repeatedly:
+ for(int i=4;i<8;i++){
+ digitalWrite(i,HIGH);
+ delay(1000);
+ digitalWrite(i,LOW);
+ }
+ for(int i=6;i>4;i--){
+ digitalWrite(i,HIGH);
+ delay(1000);
+ digitalWrite(i,LOW);
+ }
+ }
+
