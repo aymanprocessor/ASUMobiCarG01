a36884d6861e59bd881ce6e22fd1bf45702e9625
mabdelrazik <36626016+mabdelrazik@users.noreply.github.com>
2018-03-03 00:29:06 +0200

    Add files via upload

---
+++ sketch_mar02a.ino
@@ -0,0 +1,17 @@
+const byte sensor=0;
+float read;
+void setup() {
+  // put your setup code here, to run once:
+
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  read=analogRead(sensor);
+  float volt;
+  volt=(read/1024)*5000;
+  Serial.println(volt);
+  
+delay(1000);
+}
